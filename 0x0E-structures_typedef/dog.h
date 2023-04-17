1f1e4e5255666187cba577596e755c0f1dcd7c8e
Yahaya-241988 <yahayaibrahimu88@gmail.com>
2023-04-17 07:34:17 +0000

    A Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_HEADER_FILE
+#define DOG_HEADER_FILE
+
+/**
+ * struct dog - Dog Information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: This struct has no description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+
+#endif
+
+
